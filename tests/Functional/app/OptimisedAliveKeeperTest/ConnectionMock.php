b9561a98d199cf92a6f604c38aaeaaf1a024e821
Martin Fris <1735097+Rastusik@users.noreply.github.com>
2025-02-01 18:48:29 +0100

    Removed version atribute from composer (#2)

--- tests/Functional/app/OptimisedAliveKeeperTest/ConnectionMock.php
+++ tests/Functional/app/OptimisedAliveKeeperTest/ConnectionMock.php
@@ -10,29 +10,88 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Result;
 use PDO;
 
+// phpcs:disable
 /**
  * @phpstan-import-type WrapperParameterTypeArray from Connection
  */
+if (version_compare(InstalledVersions::getVersion('doctrine/dbal'), '4.0.0', '<')) {
+    final class ConnectionMock extends Connection
+    {
+        /**
+         * @var array<string>
+         */
+        private array $queries = [];
 
-final class ConnectionMock extends Connection
-{
-    private array $queries = [];
+        /**
+         * @param array<string, mixed>|list<mixed> $params
+         * @phpstan-param WrapperParameterTypeArray $types
+         */
+        public function executeQuery(
+            string $sql,
+            array $params = [],
+            $types = [],
+            ?QueryCacheProfile $qcp = null,
+        ): Result {
+            $args = func_get_args();
+            $this->queries[] = $args[0];
 
-    /**
-     * @param array<string, mixed>|list<mixed> $params
-     * @phpstan-param WrapperParameterTypeArray $types
-     */
-    public function executeQuery(
-        string $sql,
-        array $params = [],
-        array $types = [],
-        ?QueryCacheProfile $qcp = null,
-    ): Result {
-        $args = func_get_args();
-        $this->queries[] = $args[0];
+            return new class extends Result {
+                public function __construct() {}
 
-        // phpcs:disable
-        if (version_compare(InstalledVersions::getVersion('doctrine/dbal'), '4.0.0', '<')) {
+                /**
+                 * @return mixed
+                 */
+                public function fetchOne()
+                {
+                    return '1';
+                }
+
+                /**
+                 * @return mixed
+                 */
+                public function fetch($fetchMode = null, $cursorOrientation = PDO::FETCH_ORI_NEXT, $cursorOffset = 0)
+                {
+                    return 1;
+                }
+            };
+        }
+
+        /**
+         * @return array<string>
+         */
+        public function getQueries(): array
+        {
+            return $this->queries;
+        }
+
+        public function getQueriesCount(): int
+        {
+            return count($this->queries);
+        }
+    }
+} else {
+    // phpcs:enable
+    // phpcs:ignore PSR1.Classes.ClassDeclaration.MultipleClasses
+    final class ConnectionMock extends Connection
+    {
+        /**
+         * @var array<string>
+         */
+        private array $queries = [];
+
+        /**
+         * @param array<string, mixed>|list<mixed> $params
+         * @phpstan-param WrapperParameterTypeArray $types
+         */
+        public function executeQuery(
+            string $sql,
+            array $params = [],
+            array $types = [],
+            ?QueryCacheProfile $qcp = null,
+        ): Result {
+            $args = func_get_args();
+            $this->queries[] = $args[0];
+
             return new class extends Result {
                 public function __construct() {}
 
@@ -40,38 +99,20 @@ final class ConnectionMock extends Connection
                 {
                     return '1';
                 }
-
-                public function fetch(
-                    $fetchMode = null,
-                    $cursorOrientation = PDO::FETCH_ORI_NEXT,
-                    $cursorOffset = 0,
-                ): mixed {
-                    return 1;
-                }
             };
         }
 
-        // phpcs:enable
-        return new class extends Result {
-            public function __construct() {}
+        /**
+         * @return array<string>
+         */
+        public function getQueries(): array
+        {
+            return $this->queries;
+        }
 
-            public function fetchOne(): mixed
-            {
-                return '1';
-            }
-        };
-    }
-
-    /**
-     * @return array<string>
-     */
-    public function getQueries(): array
-    {
-        return $this->queries;
-    }
-
-    public function getQueriesCount(): int
-    {
-        return count($this->queries);
+        public function getQueriesCount(): int
+        {
+            return count($this->queries);
+        }
     }
 }
